148e4aabcb8655399279745437bf61431d60d734
Armno <monkeyarmno@gmail.com>
2015-02-25 17:11:22 +0700

    create Navigation component with render method which renders a list of links

--- dist/ExampleComponent.js
+++ dist/ExampleComponent.js
@@ -1,11 +1,54 @@
-var ExampleComponent = React.createClass({displayName: "ExampleComponent",
+var navigationConfig = [{
+	href: 'https://armno.github.io',
+	text: 'Armno blog'
+}, {
+	href: 'http://nemo.in.th',
+	text: 'Nemo blog'
+}];
+
+var Navigation = React.createClass({displayName: "Navigation",
+	getInitialState: function() {
+		return {
+			openDropdown: -1
+		}
+	},
+	getDefaultProps: function() {
+		return {
+			config: []
+		}
+	},
+	propTypes: {
+		config: React.PropTypes.array
+	},
+	componentWillMount: function() {
+		// called before `render()`
+	},
+	componentDidMount: function() {
+		// called after `render()`
+	},
+	componentWillUnmount: function() {
+		// called before the component is removed from the DOM
+		// useful to unbind event listeners specific to this component
+	},
 	render: function() {
+		var config = this.props.config;
+
+		var items = config.map(function(item) {
+			return (
+				React.createElement("li", {className: "navigation__item"}, 
+					React.createElement("a", {className: "navigation__link", href:  item.href}, 
+						 item.text
+					)
+				)
+			)
+		});
+
 		return (
 			React.createElement("div", {className: "navigation"}, 
-				"Hello World!"
+				items 
 			)
 		);
 	}
 });
 
-React.render(React.createElement(ExampleComponent, null), document.body);
+React.render(React.createElement(Navigation, {config: navigationConfig }), document.body);
--- jsx/ExampleComponent.jsx
+++ jsx/ExampleComponent.jsx
@@ -1,11 +1,54 @@
-var ExampleComponent = React.createClass({
+var navigationConfig = [{
+	href: 'https://armno.github.io',
+	text: 'Armno blog'
+}, {
+	href: 'http://nemo.in.th',
+	text: 'Nemo blog'
+}];
+
+var Navigation = React.createClass({
+	getInitialState: function() {
+		return {
+			openDropdown: -1
+		}
+	},
+	getDefaultProps: function() {
+		return {
+			config: []
+		}
+	},
+	propTypes: {
+		config: React.PropTypes.array
+	},
+	componentWillMount: function() {
+		// called before `render()`
+	},
+	componentDidMount: function() {
+		// called after `render()`
+	},
+	componentWillUnmount: function() {
+		// called before the component is removed from the DOM
+		// useful to unbind event listeners specific to this component
+	},
 	render: function() {
+		var config = this.props.config;
+
+		var items = config.map(function(item) {
+			return (
+				<li className="navigation__item">
+					<a className="navigation__link" href={ item.href}>
+						{ item.text }
+					</a>
+				</li>
+			)
+		});
+
 		return (
 			<div className="navigation">
-				Hello World!
+				{ items }
 			</div>
 		);
 	}
 });
 
-React.render(<ExampleComponent />, document.body);
+React.render(<Navigation config={ navigationConfig } />, document.body);
